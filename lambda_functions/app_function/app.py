24c64c6bb490a95770283f1ca999332fe972f609
Abdul Hadi <abdulhadi.work03@gmail.com>
2026-03-06 23:01:09 +0500

    Update app.py

--- lambda_functions/app_function/app.py
+++ lambda_functions/app_function/app.py
@@ -1,9 +1,10 @@
 import json
 
-def lambda_handler(event, context):
+def lambda_handler(event, context): 
     # TODO implement
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from app function! v1')
     }
+
 
