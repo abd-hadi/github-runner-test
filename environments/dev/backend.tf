407bc35512869319db0fc05bb602cc937945b9fe
Zayan Ahmed <zayan.ahmad384@gmail.com>
2026-03-06 12:12:34 +0500

    Configure S3 backend for Terraform in dev environment

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,10 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     # Per First-time-lambda-deploy.md and README: dedicated S3 bucket per environment
-#     bucket = "myorg-terraform-state-dev-yoyo" # adjust to your actual dev state bucket name
-#     key    = "environments/dev/terraform.tfstate"
-#     region = "us-east-1"                 # adjust to your dev region
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    # Per First-time-lambda-deploy.md and README: dedicated S3 bucket per environment
+    bucket = "demo-terraform-state-bucket-7832" # adjust to your actual dev state bucket name
+    key    = "environments/dev/terraform.tfstate"
+    region = "us-east-1"                 # adjust to your dev region
+    encrypt = true
+  }
+}
+
 
